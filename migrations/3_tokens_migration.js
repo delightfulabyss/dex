2391bc0918b3a72e306d20c423790984cc521336
Matthew Watman <mattwatman@gmail.com>
2021-03-09 15:52:12 -0500

    Updated post-deployment configuration activites and recompiled contracts

--- migrations/3_tokens_migration.js
+++ migrations/3_tokens_migration.js
@@ -5,6 +5,7 @@ module.exports = async function (deployer) {
   await deployer.deploy(Link);
   let wallet = await Wallet.deployed();
   let link = await Link.deployed();
+  await wallet.addTokenSupport(web3.utils.utf8ToHex('LINK'), link.address);
   await link.approve(wallet.address, 500);
   await wallet.deposit(100, web3.utils.utf8ToHex('LINK'));
 };
