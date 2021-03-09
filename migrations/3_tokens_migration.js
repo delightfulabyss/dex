bcb5756936653cfe95debfbd0584defe5ac174e3
Matthew Watman <mattwatman@gmail.com>
2021-03-09 15:56:10 -0500

    Updated post-deployment configuration activities

--- migrations/3_tokens_migration.js
+++ migrations/3_tokens_migration.js
@@ -1,11 +1,13 @@
 const Link = artifacts.require("Link");
 const Wallet = artifacts.require("Wallet");
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, accounts) {
   await deployer.deploy(Link);
   let wallet = await Wallet.deployed();
   let link = await Link.deployed();
   await wallet.addTokenSupport(web3.utils.utf8ToHex('LINK'), link.address);
   await link.approve(wallet.address, 500);
   await wallet.deposit(100, web3.utils.utf8ToHex('LINK'));
+  let balanceOfLink = await wallet.balances(accounts[0], web3.utils.utf8ToHex('LINK'));
+  console.log(balanceOfLink);
 };
