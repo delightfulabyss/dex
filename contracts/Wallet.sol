66913a2a09ae79992dcdf723ca75c4dd22bfda4d
Matthew Watman <mattwatman@gmail.com>
2021-03-09 14:08:00 -0500

    Added function headers and ERC20 interface import

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,5 +1,6 @@
 pragma solidity >= 0.6.0 < 0.8.0;
 
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol"
 contract Wallet {
     
     //In order for the dex to call the token contract to do transfer calls (support for the token)
@@ -13,4 +14,13 @@ contract Wallet {
     //Double mapping of addresses => ticker (in bytes) => balances
     //Uses bytes32 because we can't compare strings directly in Solidity
     mapping (address => mapping(bytes32 => uint256)) public balances;
+    
+    //Takes information about a token and adds it to storage
+    function addToken(bytes32 ticker, address tokenAddress) external {
+        tokenMapping[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    function deposit(uint amount, bytes32 ticker) external {}
+    function withdraw(uint amount, bytes32 ticker) external {}
 }
