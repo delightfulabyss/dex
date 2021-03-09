4bda4d94da9e144a8af9477eadc8dd7daa5ca2d2
Matthew Watman <mattwatman@gmail.com>
2021-03-09 14:42:18 -0500

    Added deposit functionality

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -20,12 +20,18 @@ contract Wallet {
     mapping (address => mapping(bytes32 => uint256)) public balances;
     
     //Takes information about a token and adds it to storage
-    function addToken(bytes32 ticker, address tokenAddress) external {
+    function addTokenSupport(bytes32 ticker, address tokenAddress) external {
         tokenMapping[ticker] = Token(ticker, tokenAddress);
         tokenList.push(ticker);
     }
 
-    function deposit(uint amount, bytes32 ticker) external {}
+    function deposit(uint amount, bytes32 ticker) external {
+        require(tokenMapping[ticker].tokenAddress != address(0), "Token is not supported at this time");
+        require(IERC20(tokenMapping[ticker].tokenAddress).getBalanceOf(msg.sender)>= amount, "Insufficient balance for deposit");
+        balances[msg.sender][ticker] = balances[msg.sender][ticker].add(amount);
+        IERC20(tokenMapping[ticker].tokenAddress).transfer(address.this, amount);
+    }
+
     function withdraw(uint amount, bytes32 ticker) external {
         require(tokenMapping[ticker].tokenAddress != address(0), "Token is not supported at this time");
         require(balances[msg.sender][ticker]>= amount, "Insufficient balance for withdrawal");
