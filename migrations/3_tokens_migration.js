9ca354caaf3bb0ebd4c4072eec8e83e3b483a114
Matthew Watman <mattwatman@gmail.com>
2021-03-09 15:47:34 -0500

    Added configuration activities post-deployment

--- migrations/3_tokens_migration.js
+++ migrations/3_tokens_migration.js
@@ -1,5 +1,10 @@
 const Link = artifacts.require("Link");
+const Wallet = artifacts.require("Wallet");
 
-module.exports = function (deployer) {
-  deployer.deploy(Link);
+module.exports = async function (deployer) {
+  await deployer.deploy(Link);
+  let wallet = await Wallet.deployed();
+  let link = await Link.deployed();
+  await link.approve(wallet.address, 500);
+  await wallet.deposit(100, web3.utils.utf8ToHex('LINK'));
 };
