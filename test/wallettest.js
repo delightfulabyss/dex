2e47df8355d1615e8f8ee9ff4611c303c3a78988
Matthew Watman <mattwatman@gmail.com>
2021-03-10 11:31:30 -0500

    Removed deployer statement

--- test/wallettest.js
+++ test/wallettest.js
@@ -1,9 +1,9 @@
 const Dex = artifacts.require("Dex");
 const Link = artifacts.require("Link");
 const truffleAssert = artifacts.require('truffle-assertions');
+
 contract("Dex", accounts => {
     it("shold only be possible for owner to add tokens", async () => {
-        await deployer.deploy(Link);
         let dex = await Dex.deployed();
         let link = await Link.deployed();
         await truffleAssert.passes(
