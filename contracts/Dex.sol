05144fa341762d64ff62da38bb4e34950807ed58
Matthew Watman <mattwatman@gmail.com>
2021-03-10 10:42:39 -0500

    Added storage variables

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,2 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >= 0.6.0 < 0.8.0;
+import "./Wallet.sol";
+
+contract Dex is Wallet {
+    
+    enum Side {
+        BUY,
+        SELL
+    }
+
+    struct Order {
+        uint id;
+        address trader;
+        Side side;
+        bytes32 ticker;
+        uint amount;
+        uint price;
+    }
+
+    mapping(bytes32 => mapping(uint => Order[]));
+}
