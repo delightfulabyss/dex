a705657ec89d0b52efe6a28a5d3f24f06130716b
Matthew Watman <mattwatman@gmail.com>
2021-03-10 11:12:16 -0500

    Changed test to referenc dex instead of wallet

--- test/wallettest.js
+++ test/wallettest.js
@@ -3,9 +3,7 @@ const Dex = artifacts.require("Dex");
 contract("Dex", accounts => {
     it("shold only be possible for owner to add tokens", async () => {
     await deployer.deploy(Link);
-    let wallet = await Wallet.deployed();
-    let link = await Link.deployed();
+    let dex = await Dex.deployed();
     await wallet.addTokenSupport(web3.utils.utf8ToHex('LINK'), link.address);
-    await link.approve(wallet.address, 500);
     })
 })
