4a74008526cd747f8df6f9d7665b4d1c7344d817
Matthew Watman <mattwatman@gmail.com>
2021-03-10 11:25:37 -0500

    Changed all references of wallet to dex

--- migrations/3_tokens_migration.js
+++ migrations/3_tokens_migration.js
@@ -1,13 +1,13 @@
 const Link = artifacts.require("Link");
-const Wallet = artifacts.require("Wallet");
+const Dex = artifacts.require("Dex");
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(Link);
-  let wallet = await Wallet.deployed();
+  let dex = await Dex.deployed();
   let link = await Link.deployed();
-  await wallet.addTokenSupport(web3.utils.utf8ToHex('LINK'), link.address);
-  await link.approve(wallet.address, 500);
-  await wallet.deposit(100, web3.utils.utf8ToHex('LINK'));
-  let balanceOfLink = await wallet.balances(accounts[0], web3.utils.utf8ToHex('LINK'));
+  await dex.addTokenSupport(web3.utils.utf8ToHex('LINK'), link.address);
+  await link.approve(dex.address, 500);
+  await dex.deposit(100, web3.utils.utf8ToHex('LINK'));
+  let balanceOfLink = await dex.balances(accounts[0], web3.utils.utf8ToHex('LINK'));
   console.log(balanceOfLink);
 };
