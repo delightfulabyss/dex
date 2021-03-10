a996e08c1a297ff3504c9fe35ed86121613dcb4d
Matthew Watman <mattwatman@gmail.com>
2021-03-10 11:14:34 -0500

    Added token approval to test

--- test/wallettest.js
+++ test/wallettest.js
@@ -1,9 +1,12 @@
 const Dex = artifacts.require("Dex");
+const Link = artifacts.require("Link");
 
 contract("Dex", accounts => {
     it("shold only be possible for owner to add tokens", async () => {
-    await deployer.deploy(Link);
-    let dex = await Dex.deployed();
-    await dex.addTokenSupport(web3.utils.utf8ToHex('LINK'), link.address);
+        await deployer.deploy(Link);
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        await link.approve(dex.address, 500);
+        await dex.addTokenSupport(web3.utils.utf8ToHex('LINK'), link.address);
     })
 })
