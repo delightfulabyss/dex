b21cf1ecb45b024b712b720bc75034aa17d12a7c
Matthew Watman <mattwatman@gmail.com>
2021-03-10 11:19:29 -0500

    Added assert statement

--- test/wallettest.js
+++ test/wallettest.js
@@ -1,11 +1,13 @@
 const Dex = artifacts.require("Dex");
 const Link = artifacts.require("Link");
-
+const truffleAssert = artifacts.require('truffle-assertions');
 contract("Dex", accounts => {
     it("shold only be possible for owner to add tokens", async () => {
         await deployer.deploy(Link);
         let dex = await Dex.deployed();
         let link = await Link.deployed();
-        await dex.addTokenSupport(web3.utils.utf8ToHex('LINK'), link.address, {from: accounts[0]});
-    })
-})
+        truffleAssert.passes(
+            dex.addTokenSupport(web3.utils.utf8ToHex('LINK'), link.address, { from: accounts[0] })
+        );
+    });
+});
