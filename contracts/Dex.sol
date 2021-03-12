475051e44740ff9edf5af513687c6736c0378c01
Matthew Watman <mattwatman@gmail.com>
2021-03-12 11:40:17 -0500

    Added sort functionality to limit order function

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -20,6 +20,9 @@ contract Dex is Wallet {
         uint amount;
         uint price;
     }
+
+    uint public nextOrderId;
+
     //Ticker => Side => Orderbook
     mapping(bytes32 => mapping(uint => Order[])) public orderBook;
 
@@ -28,11 +31,43 @@ contract Dex is Wallet {
     }
 
     function createLimitOrder(Side _side, bytes32 _ticker, uint _amount, uint _price) public {
+    //Perform checks as required in tests 
         if(_side == Side.BUY){
             require(balances[msg.sender]['ETH'] >= _amount.mul(_price));
         }
         if(_side == Side.SELL){
             require(balances[msg.sender][_ticker] >= _amount);
         }
+        
+        Order[] storage orders = orderBook[ticker][uint(_side)];
+        orders.push(
+            Order(nextOrderId, msg.sender, _side, _ticker, _amount, _price)
+        );
+        //Bubble sort algorithm [1, 2, 3, 4, 5, 6. 7, 8, 9 , 10]
+        if(_side == Side.BUY){
+            for (uint256 index = orders.length; index > 0; index--) {
+                if(orders[index].price > orders[index - 1].price){
+                    //swap element[index] and element[index - 1]
+                    Order memory temp = orders[index];
+                    orders[index] = orders[index - 1];
+                    orders[index - 1] = temp;
+                } else {
+                    continue;
+                }
+            }
+        }
+        else if(_side == Side.SELL){
+            for (uint256 index = orders.length; index > 0; index--) {
+                if(orders[index].price < orders[index - 1].price){
+                    //swap last element and penultimate element
+                    Order memory temp = orders[index];
+                    orders[index] = orders[index - 1];
+                    orders[index - 1] = temp;
+                } else {
+                    continue;
+                }
+            }
+        }
+        nextOrderId++;
     }
 }
