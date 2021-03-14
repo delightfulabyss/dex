d0f6dd2bd4e13026d76a8fff4a5a8bc03fbf46e6
Matthew Watman <mattwatman@gmail.com>
2021-03-14 11:28:33 -0400

    Added test functionality

--- test/dextest.js
+++ test/dextest.js
@@ -101,15 +101,37 @@ contract("Dex", accounts => {
     });
     //When creating a buy market order, the buyer needs to have enough ETH for the trade
     it("should throw an error if the buyer does not have enough ETH for the buy market order", async () => {
-
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        let balance = await dex.balances(accounts[0], ETH_TICKER);
+        assert(balance.toNumber(), 0, "Initial ETH balance is not 0");
+        await truffleAssert.reverts(
+            dex.createMarketOrder(BUY_SIDE, LINK_TICKER, 10)
+        );
     });
     
     it("should pass if the buyer has enough ETH for the buy market order", async () => {
-
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        dex.depositEth({ value: web3.utils.toWei('5', 'ether') });
+        let balance = await dex.balances(accounts[0], ETH_TICKER);
+        assert(balance.toNumber(), 5, "Initial ETH balance is not 5");
+        await truffleAssert.reverts(
+            dex.createMarketOrder(BUY_SIDE, LINK_TICKER, 10)
+        );
     });
     
     //Market orders can be submitted even if the order book is empty
-    it("should pass even if the market order book is empty", async () => {});
+    it("should pass even if the market order book is empty", async () => {
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        let buyOrderBook = await dex.getOrderBook(LINK_TICKER, 0);
+        assert.equal(sellOrderBook, [], "Sell orderbook is not empty");
+        await truffleAssert.passes(
+            dex.createMarketOrder(BUY_SIDE, LINK_TICKER, 10)
+        );
+    });
+
     
     //Market orders should be filled until the order book is empty or the market order is 100% filled
     it("", async () => {
